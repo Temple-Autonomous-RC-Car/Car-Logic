c10c47dc46c5a24c6b905f48f3f19af82a88880f
Pratik <pratik1497@gmail.com>
2018-10-23 19:50:22 -0400

    plz

--- Sensor Test/gyrotest.py
+++ Sensor Test/gyrotest.py
@@ -11,21 +11,30 @@ from time import sleep
 
 # Create a new instance of the MPU6050 class
 sensor = MPU6050(0x68)
-
+highestOutX = 0
+highestOutY = 0
+highestOutZ = 0
 while True:
     accel_data = sensor.get_accel_data()
     gyro_data = sensor.get_gyro_data()
     temp = sensor.get_temp()
 
     print("Accelerometer data")
-    print("x: " + str(accel_data['x']))
-    print("y: " + str(accel_data['y']))
-    print("z: " + str(accel_data['z']))
-
-    print("Gyroscope data")
-    print("x: " + str(gyro_data['x']))
-    print("y: " + str(gyro_data['y']))
-    print("z: " + str(gyro_data['z']))
-
-    print("Temp: " + str(temp) + " C")
-    sleep(2)
+    #print("x: " + str(accel_data['x']))
+    #print("y: " + str(accel_data['y']))
+    #print("z: " + str(accel_data['z']))
+    if(abs(accel_data['x']) > abs(highestOutX)):
+        highestOutX = accel_data['x']
+    if(abs(accel_data['y']) > abs(highestOutY)):
+        highestOutY = accel_data['y']
+    if(abs(accel_data['z']) > abs(highestOutZ)):
+        highestOutZ = accel_data['z']
+   # print("Gyroscope data")
+   # print("x: " + str(gyro_data['x']))
+   # print("y: " + str(gyro_data['y']))
+   # print("z: " + str(gyro_data['z']))
+    print("x: "+ str(highestOutX))
+    print("y: "+ str(highestOutY))
+    print("z: "+ str(highestOutZ))
+   #print("Temp: " + str(temp) + " C")
+    
