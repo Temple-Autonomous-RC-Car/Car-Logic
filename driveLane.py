492142c61328ac48d2d4d8c86d5926cfe0eb3d09
Pratik <pratik1497@gmail.com>
2018-11-11 15:39:29 -0500

    Update drive logic

--- driveLane.py
+++ driveLane.py
@@ -5,26 +5,40 @@ steer(0)
 drive(0)
 
 
-leftSteer = -.60
-rightSteer = .60
-centerSteer = .12
+c = "CENTER"
+l = "LEFT"
+r = "RIGHT"
+
+leftSteer = 1
+rightSteer = -1
+centerSteer = 0
 
 live_image.run()
 try:
+    drive(.3)
     while(True):
-        drive(.28)
         angle = live_image.liveAngle
-        print("Got angle of %d" % (angle))
-        if(angle > -88 and angle < 0):
-            steer(leftSteer)
-            sleep(.2)
-        elif(angle < 88 and angle >0):
-            steer(rightSteer)
-            sleep(.2)
+        botOfLine = live_image.liveLine
+        #print("Got angle of %d" % (angle))
+        if(angle > -87 and angle < 0):
+            #drive(.26)
+            if(botOfLine == c or botOfLine == l): 
+                steer(leftSteer)
+            elif(botOfLine == r):
+                steer(rightSteer)
+            #sleep(.2)
+        elif(angle < 87 and angle >0):
+            #drive(.26)
+            if(botOfLine == l): 
+                steer(leftSteer)
+            elif(botOfLine == c or botOfLine == r):
+                steer(rightSteer)
+            #sleep(.2)
         else:
-            steer(0)
-            sleep(.2)
+            steer(centerSteer)
+            #drive(.28)
+            #sleep(.2)
 except KeyboardInterrupt:
     steer(0)
-    drive(0)
+    drive(-1)
     exit()
